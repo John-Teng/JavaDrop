c9f8e687e4eb5ac27ed39154e7d51e83956c0b90
John Teng <john.teng97@gmail.com>
2020-05-22 16:42:53 -0400

    Added 2nd part of the transaction

--- src/main/java/ClientProcessor.java
+++ src/main/java/ClientProcessor.java
@@ -1,3 +1,4 @@
+import com.google.common.net.InetAddresses;
 import lombok.extern.log4j.Log4j2;
 import org.apache.commons.lang3.math.NumberUtils;
 
@@ -9,6 +10,7 @@ import java.net.Socket;
 @Log4j2
 public class ClientProcessor {
     private static final String DELIMITER = "/"; // TODO check if this acts as a regex
+    private static final String OK_RESPONSE = "OK";
     @Nonnull
     protected final Socket csock;
     @Nullable
@@ -16,6 +18,7 @@ public class ClientProcessor {
     @Nullable
     protected DataOutputStream out;
 
+    // TODO make this class a singleton
     public ClientProcessor(@Nonnull Socket sock) {
         csock = sock;
         log.debug("Connected to client from IP " + csock.getRemoteSocketAddress() + " Port " + csock.getPort());
@@ -42,11 +45,22 @@ public class ClientProcessor {
         }
     }
 
-    private boolean validIOStreams() {
+    private boolean isIOStreamValid() {
         return in != null && out != null;
     }
 
-    private boolean isValidTransferRequest() {
+    private boolean isValidTransferRequest(@Nonnull String[] parts) {
+        return parts.length == 3
+                && NumberUtils.isParsable(parts[1])
+                && InetAddresses.isInetAddress(parts[2]);
+    }
+
+    public void processClient() {
+        if (!isIOStreamValid()) {
+            closeConnections();
+            return;
+        }
+
         // step 1: parse the filename/filesize(in bytes)/ip from the connection as chars
         final StringBuilder sb = new StringBuilder();
         try {
@@ -55,33 +69,31 @@ public class ClientProcessor {
             }
         } catch (IOException e) {
             e.printStackTrace();
-            return false;
+            closeConnections();
+            return;
         }
         final String[] parts = sb.toString().split(DELIMITER);
-        if (parts.length != 3) {
-            return false;
+        if (!isValidTransferRequest(parts)) {
+            closeConnections();
+            return;
         }
-        final String filename = parts[0], ip = parts[2];
-        if (!NumberUtils.isParsable(parts[1]))
-            return false;
+        final String filename = parts[0], host = parts[2];
         final int filesize = Integer.parseInt(parts[1]);
+        // TODO Ask user with prompt to request transfer of file
 
-        // send a prompt to the user to see if they would like to accept this file
-        return true; // or false based on user's input
-    }
 
-    public void processClient() {
-        if (!validIOStreams()) {
-            closeConnections();
-            return;
-        }
-
-        if (!isValidTransferRequest()) {
-            closeConnections();
-            return;
-        }
         // step 2: If valid respond with "OK"
+        try {
+            out.writeChars(OK_RESPONSE);
+        } catch (IOException e) {
+            e.printStackTrace();
+            closeConnections();
+            return;
+        }
+
         // step 3: parse the binary data and keep count
         // step 4: once the connection severs, count the binary data to see if complete
+
+        closeConnections();
     }
 }
