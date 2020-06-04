153f8381381f2c6ee48e6606ad47c29d51181076
John Teng <john.teng97@gmail.com>
2020-06-04 17:26:46 -0400

    added unit test for ServerProcessor

---
+++ src/test/java/ServerProcessorTest.java
@@ -0,0 +1,26 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.*;
+
+import static org.mockito.Mockito.mock;
+import static org.junit.Assert.*;
+
+public class ServerProcessorTest {
+    public ServerProcessor processor;
+    
+    @Before
+    public void init() {
+        processor = new ServerProcessor(mock(File.class), mock(String.class));
+    }
+
+    @Test
+    public void testGenerateRequestStringSuccess() {
+        final String expected = "test.png/123456/192.241.41.16%";
+        final String filename = "test.png", host = "192.241.41.16";
+        long filesize = 123456;
+
+        assertEquals(expected, processor.generateRequestString(filename, host, filesize));
+    }
+
+}
