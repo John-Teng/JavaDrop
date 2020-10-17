52170df154e35e312211597cb098827f2d3c924b
John Teng <john.teng97@gmail.com>
2020-10-17 18:05:24 -0400

    Implements a basic dialog structure to provide yes/no user input

--- src/main/java/JavaFxDialog.java
+++ src/main/java/JavaFxDialog.java
@@ -10,58 +10,33 @@ import javafx.stage.Stage;
 import lombok.extern.log4j.Log4j2;
 
 import javax.annotation.Nonnull;
-import java.util.concurrent.CountDownLatch;
 
 @Log4j2
 public class JavaFxDialog extends Application {
+    // Configuration
+    private static final int BUTTON_WIDTH = 60;
+    private static final int DIALOG_WIDTH = 300;
+    private static final int DIALOG_HEIGHT = 125;
+    private static final int BUTTON_SPACING = 30;
+    private static final int LAYOUT_SPACING = 10;
+    private static final String ACCEPT_BUTTON_TEXT = "Yes";
+    private static final String REJECT_BUTTON_TEXT = "No";
+    private static final String TITLE = "JavaDrop";
+
     private final String filename, source;
     private final long filesize;
     private final Callback callback;
 
     interface Callback {
         void success();
+
         void failure();
     }
 
-    static class DialogArbiter implements Permissions.Arbiter {
-        @Override
-        public void requestPermission(@Nonnull final String filename,
-                                      @Nonnull final String source,
-                                      final long filesize,
-                                      @Nonnull final Permissions.Requester requester) {
-            final CountDownLatch latch = new CountDownLatch(1);
-            final int[] returnValues = new int[1];
-            try {
-                new Thread(() -> {
-                    log.debug("Spawned thread on: " + Thread.currentThread().getId());
-                    Platform.runLater(() -> {
-                        new JavaFxDialog(filename, source, filesize, new Callback() {
-                            @Override
-                            public void success() {
-                                returnValues[0] = 1;
-                                latch.countDown();
-                            }
-
-                            @Override
-                            public void failure() {
-                                returnValues[0] = 0;
-                                latch.countDown();
-                            }
-                        }).start(new Stage());
-                    });
-                }).start();
-                latch.await();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            log.debug("Return value: " + returnValues[0]);
-        }
-    }
-
-    public JavaFxDialog (@Nonnull final String filename,
-                         @Nonnull final String source,
-                         final long filesize,
-                         @Nonnull final Callback callback) {
+    public JavaFxDialog(@Nonnull final String filename,
+                        @Nonnull final String source,
+                        final long filesize,
+                        @Nonnull final Callback callback) {
         this.filename = filename;
         this.source = source;
         this.filesize = filesize;
@@ -71,35 +46,37 @@ public class JavaFxDialog extends Application {
     public static void main(String[] args) {
         DialogArbiter d = new DialogArbiter();
         System.out.println("Main thread id: " + Thread.currentThread().getId());
-        d.requestPermission("test.png", "192.12.31.1", 123123, new Permissions.Requester() {
+        boolean permission = d.requestPermission("test.png", "192.12.31.1", 123123, new Permissions.Requester() {
             @Override
             public void onPermissionGranted() {
                 System.out.println("GRANTED");
-                System.out.println("Executing thread id: " + Thread.currentThread().getId());
+                System.out.println("Permission granted on thread id: " + Thread.currentThread().getId());
             }
 
             @Override
             public void onPermissionRejected() {
                 System.out.println("REJECTED");
-                System.out.println("Executing thread id: " + Thread.currentThread().getId());
+                System.out.println("Permission rejected on thread id: " + Thread.currentThread().getId());
             }
         });
+        log.debug("Is permission granted: " + permission);
     }
 
     @Override
     public void start(Stage stage) {
-        final Label label = new Label("Accept file transfer of file: " + filename + "(" + filesize + ") bytes from " + source + "?");
-        final Button reject = new Button("No");
-        reject.setMinWidth(40);
-        reject.setMaxWidth(40);
+        final Label topLabel = new Label("Accept file transfer of file: " + filename + " (" + (filesize / 1024) + " KB)");
+        final Label bottomLabel = new Label("From " + source + "?");
+        final Button reject = new Button(REJECT_BUTTON_TEXT);
+        reject.setMinWidth(BUTTON_WIDTH);
+        reject.setMaxWidth(BUTTON_WIDTH);
         reject.setOnAction(e -> {
             callback.failure();
             Platform.exit();
         });
 
-        final Button accept = new Button("Yes");
-        accept.setMinWidth(40);
-        accept.setMaxWidth(40);
+        final Button accept = new Button(ACCEPT_BUTTON_TEXT);
+        accept.setMinWidth(BUTTON_WIDTH);
+        accept.setMaxWidth(BUTTON_WIDTH);
         accept.setOnAction(e -> {
             callback.success();
             Platform.exit();
@@ -108,16 +85,16 @@ public class JavaFxDialog extends Application {
         final HBox buttonLayout = new HBox();
         buttonLayout.getChildren().addAll(accept, reject);
         buttonLayout.setAlignment(Pos.CENTER);
-        buttonLayout.setSpacing(20);
+        buttonLayout.setSpacing(BUTTON_SPACING);
 
         final VBox layout = new VBox();
-        layout.getChildren().addAll(label, buttonLayout);
+        layout.getChildren().addAll(topLabel, bottomLabel, buttonLayout);
         layout.setAlignment(Pos.CENTER);
-        layout.setSpacing(20);
+        layout.setSpacing(LAYOUT_SPACING);
 
-        final Scene scene = new Scene(layout, 150, 100);
+        final Scene scene = new Scene(layout, DIALOG_WIDTH, DIALOG_HEIGHT);
         stage.setScene(scene);
-        stage.setTitle("JavaDrop");
+        stage.setTitle(TITLE);
         stage.show();
         System.out.println("Dialog thread id: " + Thread.currentThread().getId());
     }
--- src/main/java/Permissions.java
+++ src/main/java/Permissions.java
@@ -2,7 +2,7 @@ import javax.annotation.Nonnull;
 
 public class Permissions {
     interface Arbiter {
-        void requestPermission(@Nonnull final String filename,
+        boolean requestPermission(@Nonnull final String filename,
                                @Nonnull final String source,
                                final long filesize,
                                @Nonnull final Requester requester);
